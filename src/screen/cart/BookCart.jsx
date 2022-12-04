6575989bf5702bb8db996c59afe12d3c34cb152f
oberghub <nataponball@hotmail.com>
2022-12-04 15:12:23 +0700

    แก้เล็กน้อย

--- src/screen/cart/BookCart.jsx
+++ src/screen/cart/BookCart.jsx
@@ -9,28 +9,15 @@ export const BookCart = () => {
         {
             title: 'Think Python : How to Think Like a Computer Scientist 2nd Edition',
             desc: 'Think Python is an introduction to Python programming for beginners. It starts with basic concepts of programming; it is carefully designed to define all terms when they are first used and to develop each new concept in a logical progression. Larger pieces, like recursion and object-oriented programming, are divided into a sequence of smaller steps and introduced over the course of several chapters.',
-            img: '../local_image/book_ex.jpg'
+            img: '../local_image/book_ex.jpg',
+            quantity : 2
         },
         {
             title: 'Think Java: How to Think Like a Computer Scientist 2nd Edition',
             desc: 'Think Java is a hands-on introduction to computer science and programming used by many universities and high schools around the world. Its conciseness, emphasis on vocabulary, and informal tone make it particularly appealing for readers with little or no experience. The book starts with the most basic programming concepts and gradually works its way to advanced object-oriented techniques. In this fully updated and expanded edition, authors Allen Downey and Chris Mayfield introduce programming as a means for solving interesting problems. Each chapter presents material for one week of a college course and includes exercises to help you practice what you have learned. Along the way, you’ll see nearly every topic required for the AP Computer Science A exam and Java SE Programmer I certification.',
-            img: '../local_image/think_java_covr_large.png'
+            img: '../local_image/think_java_covr_large.png',
+            quantity : 1
         },
-        {
-            title: 'Think Python : How to Think Like a Computer Scientist 2nd Edition',
-            desc: 'Think Python is an introduction to Python programming for beginners. It starts with basic concepts of programming; it is carefully designed to define all terms when they are first used and to develop each new concept in a logical progression. Larger pieces, like recursion and object-oriented programming, are divided into a sequence of smaller steps and introduced over the course of several chapters.',
-            img: '../local_image/book_ex.jpg'
-        },
-        {
-            title: 'Think Java: How to Think Like a Computer Scientist 2nd Edition',
-            desc: 'Think Java is a hands-on introduction to computer science and programming used by many universities and high schools around the world. Its conciseness, emphasis on vocabulary, and informal tone make it particularly appealing for readers with little or no experience. The book starts with the most basic programming concepts and gradually works its way to advanced object-oriented techniques. In this fully updated and expanded edition, authors Allen Downey and Chris Mayfield introduce programming as a means for solving interesting problems. Each chapter presents material for one week of a college course and includes exercises to help you practice what you have learned. Along the way, you’ll see nearly every topic required for the AP Computer Science A exam and Java SE Programmer I certification.',
-            img: '../local_image/think_java_covr_large.png'
-        },
-        {
-            title: 'Think Python : How to Think Like a Computer Scientist 2nd Edition',
-            desc: 'Think Python is an introduction to Python programming for beginners. It starts with basic concepts of programming; it is carefully designed to define all terms when they are first used and to develop each new concept in a logical progression. Larger pieces, like recursion and object-oriented programming, are divided into a sequence of smaller steps and introduced over the course of several chapters.',
-            img: '../local_image/book_ex.jpg'
-        }
     ])
     const openModal = () => {
         setIsActiveModal(true)
@@ -56,8 +43,8 @@ export const BookCart = () => {
                 </>
                 : null}
             <div className="w-full h-screen m-auto flex relative">
-                <div className="md:w-[70%] p-10 m-auto md:border-r-[1px] md:border-gray-200">
-                    <p className="text-4xl Gentium-B-font"><b>My Cart</b></p>
+                <div className="md:w-[70%] p-10 md:border-r-[1px] md:border-gray-200">
+                    <p className="text-4xl Gentium-B-font">My Cart</p>
                     <div className="mt-5 w-full grid grid-cols-1 min-[440px]:grid-cols-2 gap-3 md:block">
                         {BookData[0].map((item, index) =>
                             <>
@@ -65,7 +52,7 @@ export const BookCart = () => {
                                     <img src={require('../../local_image/think_python.png')} className="w-full md:w-[180px] md:h-[220px]" alt="profile-pic" />
                                     <div className="w-full md:w-[70%] p-5 md:m-0 m-auto">
                                         <p className="text-xl line-clamp-4 lg:text-2xl">{item.title}</p>
-                                        <p className="text-xl mt-5">x1</p>
+                                        <p className="text-xl mt-5">x{item.quantity}</p>
                                     </div>
                                 </div>
                             </>
@@ -75,21 +62,19 @@ export const BookCart = () => {
                 <div className="w-full md:w-[500px] p-10 bg-white fixed md:static left-0 bottom-0 z-30">
                     <p className="Gentium-B-font text-4xl">Cart Totals</p>
                     <div className="mt-5">
+                        {BookData[0].map((item, index) => 
+                        <>
                         <div className="relative flex">
-                            <p className="text-xl Gentium-B-font">Think Python</p>
-                            <p className="absolute right-0">x1</p>
+                            <div className="w-[350px]">
+                                <p className="text-xl Gentium-B-font line-clamp-1">{item.title}</p>
+                            </div>
+                            <p className="absolute right-0">x{item.quantity}</p>
                         </div>
-                        <div className="relative flex">
-                            <p className="text-xl Gentium-B-font">Think Java</p>
-                            <p className="absolute right-0">x1</p>
-                        </div>
-                        <div className="relative flex">
-                            <p className="text-xl Gentium-B-font">คำคมกระตุกจิตกระชากใจ</p>
-                            <p className="absolute right-0">x2</p>
-                        </div>
+                        </>
+                        )}
                         <div className="mt-5 relative flex">
                             <p className="text-xl Gentium-B-font">Total</p>
-                            <p className="absolute right-0">x4</p>
+                            <p className="absolute right-0">x{BookData[0].map(item => item.quantity).reduce((a, b) => a+b)}</p>
                         </div>
                     </div>
                     <div className="w-[100%] h-[50px] flex items-center justify-center bg-black mt-5 cursor-pointer"
