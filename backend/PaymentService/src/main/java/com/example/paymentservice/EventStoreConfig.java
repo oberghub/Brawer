6c9c6c715e02dbe7764741ed49253c1a975b7a9a
artist0123 <napatart1111@gmail.com>
2022-12-23 16:14:59 +0700

    add mongoevent

---
+++ backend/PaymentService/src/main/java/com/example/paymentservice/EventStoreConfig.java
@@ -0,0 +1,32 @@
+package com.example.paymentservice;
+
+import com.mongodb.client.MongoClient;
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.axonframework.extensions.mongo.DefaultMongoTemplate;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
+import org.axonframework.serialization.json.JacksonSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EventStoreConfig {
+    @Bean
+    public EmbeddedEventStore eventStore(EventStorageEngine storageEngine,
+                                         org.axonframework.config.Configuration configuration) {
+        return EmbeddedEventStore.builder()
+                .storageEngine(storageEngine)
+                .messageMonitor(configuration.messageMonitor(EventStore.class,
+                        "paymentEventStore"))
+                .build();
+    }
+    @Bean
+    public EventStorageEngine storageEngine(MongoClient client) {
+        return MongoEventStorageEngine.builder()
+                .mongoTemplate(DefaultMongoTemplate.builder().mongoDatabase(client).build())
+                .eventSerializer(JacksonSerializer.defaultSerializer())
+                .snapshotSerializer(JacksonSerializer.defaultSerializer())
+                .build();
+    }
+}
